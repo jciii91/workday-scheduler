f4b3147088e4d6b59c3746ca2248df371602ef85
JCIII <jcostello711@gmail.com>
2022-02-05 14:47:33 -0500

    event descriptions can be entered

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,4 +5,18 @@ function getTodaysDate() {
     currentDateEl.textContent = today;
 }
 
+$(".container").on("click",".description",function(){
+    timeDiv = this;
+    timeDivChild = this.childNodes[0];
+    if (timeDivChild.tagName != "P") {
+        console.log("no p")
+        return;
+    }
+
+    timeDiv.removeChild(timeDivChild);
+    descriptionInput = document.createElement("textarea");
+    descriptionInput.setAttribute("autofocus","true");
+    timeDiv.appendChild(descriptionInput);
+})
+
 getTodaysDate();
