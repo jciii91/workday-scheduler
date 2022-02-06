f0266f00e8616461e42092f4cb70c56c475c0ff0
JCIII <jcostello711@gmail.com>
2022-02-05 22:24:09 -0500

    comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
+// variables and HTML element for date display
 let todaysDate = moment();
 let dateOnly = todaysDate.format("dddd, MMMM Do YYYY");
 let timeOnly = todaysDate.format("HH");
 currentDateEl = document.getElementById("currentDay");
 
+// object for localStorage
 let myEvents = {
     "9AM" : "",
     "10AM" : "",
@@ -15,6 +17,7 @@ let myEvents = {
     "5PM" : ""
 };
 
+// Checks every 10 minutes which hour it is currently
 setInterval(function() {
     todaysDate = moment();
     dateOnly = todaysDate.format("dddd, MMMM Do YYYY");
@@ -22,9 +25,12 @@ setInterval(function() {
     checkHour();
 },600000);
 
+// function to update row colors
 function checkHour() {
+    // find all description divs
     allDescriptions = $(".container").find(".description");
     hoursArray = [9,10,11,12,13,14,15,16,17];
+    // compare hour the description corresponds to with what hour it is currently
     for (let i = 0; i<allDescriptions.length; i++) {
         if (timeOnly-hoursArray[i] > 0) {
             allDescriptions[i].className = "description past col-10 py-2 d-flex justify-content-start";
@@ -36,9 +42,13 @@ function checkHour() {
     }
 }
 
+//loads saved schedule on page load
 function loadSchedule() {
+    // parse stored schedule
     myEvents = JSON.parse(localStorage.getItem("workDaySchedule"));
+    // find all description divs
     allDescriptions = $(".container").find(".description");
+    // iterate over each row and fill in saved events
     for (let i = 0; i<allDescriptions.length; i++) {
         console.log(allDescriptions[i].childNodes[0].innerText);
         console.log(myEvents[allDescriptions[i].id.toUpperCase()]);
@@ -46,6 +56,7 @@ function loadSchedule() {
     }
 }
 
+// listener for entering events into description
 $(".container").on("click",".description",function(){
     timeDiv = this;
     timeDivChild = this.childNodes[0];
@@ -55,10 +66,12 @@ $(".container").on("click",".description",function(){
 
     innerText = "";
 
+    // check for pre-existing text
     if (timeDivChild.innerHTML != "") {
         innerText = timeDivChild.innerHTML;
     }
 
+    // remove existing p element and replace with textarea
     timeDiv.removeChild(timeDivChild);
     descriptionInput = document.createElement("textarea");
     descriptionInput.innerHTML = innerText;
@@ -66,6 +79,7 @@ $(".container").on("click",".description",function(){
     descriptionInput.focus();
 })
 
+// when textarea loses focus switch it back to a p element
 $(".container").on("blur","textarea",function(){
     timeDiv = this.parentElement;
     descriptionText = this.value.trim();
@@ -77,6 +91,7 @@ $(".container").on("blur","textarea",function(){
     timeDiv.appendChild(descriptionPara);
 })
 
+// when the save button for a row is click then save the text shown to localStorage
 $(".container").on("click",".saveBtn",function(){
     parentDiv = this.parentElement;
     timeString = parentDiv.childNodes[1].innerText;
@@ -84,6 +99,7 @@ $(".container").on("click",".saveBtn",function(){
     localStorage.setItem("workDaySchedule",JSON.stringify(myEvents));
 })
 
+// load date, update row colors, load pre-existing schedule
 currentDateEl.textContent = dateOnly;
 checkHour();
 loadSchedule();
